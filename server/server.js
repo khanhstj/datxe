ce9cc3efb605eb5180319c2998021e367959ca3d
khanhstj <khanhtran.95.k@gmail.com>
2019-07-12 13:40:37 +0700

    Cập nhật phần SocketIO trên server, chưa hoàn hiện được phần bác tài từ chối yêu cầu

--- server/server.js
+++ server/server.js
@@ -5,14 +5,11 @@ const bodyParser = require('body-parser')
 const expressValidator = require('express-validator')
 const routes = require('./src/routes/routes')
 const mongoose = require('mongoose')
-const cors = require('cors'); 
-const fileUpload = require('express-fileupload');
-const path = require('path');
-const session = require('express-session');
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const session = require('express-session')
 const http = require('http')
-const axios = require('axios')
-var _findIndex = require('lodash/findIndex')
-var MongoStore = require('connect-mongo')(session);
+var MongoStore = require('connect-mongo')(session)
 const geolib = require('geolib')
 
 const app = express()
@@ -22,23 +19,23 @@ const io = socketIO(server)
 
 
 const PORT = process.env.PORT || 8797
-const db = mongoose.connection;
- 
+const db = mongoose.connection
+
 dotenv.config()
 
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'))
+//app.set('view engine', 'jade')
 
 //Connect DB
 mongoose.set('useCreateIndex', true)
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }).then(() => console.log('DB Connected!'));
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }).then(() => console.log('DB Connected!'))
 db.on('error', (err) => {
-    console.log('DB connection error:', err.message);
+    console.log('DB connection error:', err.message)
 })
  
 app.use(morgan("dev"))
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(fileUpload());
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(fileUpload())
 app.use(cors())
 app.use(bodyParser.json())
 app.use(expressValidator())
@@ -51,143 +48,135 @@ app.use(session({
     store: new MongoStore({
         mongooseConnection: db
       })
-}));
+}))
 
 app.use('/', routes)
 
 var bacTaiTrucTuyen = []
 
 io.on('connection', function(socket) {
-    
     console.log(socket.id + ': connected')
-    
-    //console.log('Số lượng bác tài đang ở trạng thái nhận xe: ' + bacTaiTrucTuyen.length)
+
     socket.on('disconnect', function() {
         console.log(socket.id + ': disconnected')
-        //$index = _findIndex(bacTaiTrucTuyen, ['id', socket.id]);
-        //bacTaiTrucTuyen.splice($index, 1);
-        //console.log('Số lượng bác tài trực tuyến: ' + bacTaiTrucTuyen.length)
-        
+        xoaKhoiDanhSach(socket.id)        
     })
-    socket.on('tructuyen', data => {
+
+    socket.on('chayxe', data => {
         console.log('chayxe')
         console.log(data)
         bacTaiTrucTuyen.push({
             id: socket.id,
-            username: data.username,
-            sdt: data.sdt,
-            viDo: data.viDo,
-            kinhDo: data.kinhDo,
+            sdt_BT: data.sdt_BT, //username cũng là số điện thoại của bác tài
+            viDo_BT: data.viDo_BT,
+            kinhDo_BT: data.kinhDo_BT,
+            hoTen_BT: data.hoTen_BT,
+            soXe_BT: data.soXe_BT,
         })
-        console.log('Số lượng bác tài đang ở trạng thái nhận xe: ' + bacTaiTrucTuyen.length)
+
+        console.log('Số lượng bác tài trực tuyến: ' + bacTaiTrucTuyen.length)
         console.log('Danh sách bác tài')
         bacTaiTrucTuyen.forEach((item, index, array) => {
             console.log(item, index)
         })
-        
+    })
+
+    socket.on('tamdung', data => {
+        console.log('tamdung')
+        console.log(socket.id)
+        xoaKhoiDanhSach(socket.id)
+        console.log('Số lượng bác tài trực tuyến: ' + bacTaiTrucTuyen.length)
+        bacTaiTrucTuyen.forEach((item, index, array) => {
+            console.log(item, index)
+        })
     })
     
     socket.on('nhanchuyen_boqua', data => {
+		console.log('Nhận chuyến_bỏ qua')
         if(data.luaChon === true) {
-            console.log('nhanchuyen')
+			console.log('Lựa chọn là true')
+			console.log(data.id_KH)
+            io.to(data.id_KH).emit('bactainhanchuyen', {
+                hoTen_BT: data.hoTen_BT,
+                sdt_BT: data.sdt_BT,
+                soXe_BT: data.soXe_BT,
+            })
+
         }
         else if (data.luaChon === false) {
             console.log('boqua')
         }
-        
     })
+    
 
     //Phần của khách đặt xe
     socket.on('datxe', data => {
-        let danhSachBacTai = []
+		console.log('datxe')
+        var danhSachBacTai = []
+		let id_KH = socket.id
+		console.log(id_KH)
         console.log(data)
         
-        bacTaiTrucTuyen.forEach(function(item, index, array) {
-            let khoangCachBacTaiVaKH = geolib.getDistance(    
-                { latitude: data.viDoDi, longitude: data.kinhDoDi },
-                { latitude: item.viDo, longitude: item.kinhDo }
-            )
-            danhSachBacTai.push({
-                id: socket.id,
-                username: item.username,
-                viDo: item.viDo,
-                kinhDo: item.kinhDo,
-                khoangCach: khoangCachBacTaiVaKH,
-            })
-        })
-        var bacTaiGanNhat = TimBacTaiGanNhat(danhSachBacTai)
-
-        io.to(bacTaiGanNhat.id).emit('phanhoi', {
-            sdtBacTai: '0337301998',
-        })
-
-        /*
-        var khoangCach = geolib.getDistance(    
-            { latitude: data.viDoDi, longitude: data.kinhDoDi },
-            { latitude: data.viDoDen, longitude: data.kinhDoDen }
-        )
-        console.log('Khoảng cách: ' + khoangCach)
-        */
-        
-        /*
-        socket.emit('phanhoi', {
-            hoTenBacTai: 'Trần Duy Khánh',
-            sdtBacTai: '0337301998',
-            soXe: '59N2 62823',
-        })*/
-        //io.to(bacTaiTrucTuyen[0].id).emit('yeucaudatxe', data)
-        /*
-        let danhSachKhoangCach = []
-        console.log(data)
-        console.log(bacTaiTrucTuyen[0] + 'bac tai truc tuyen dau tien')
         if(bacTaiTrucTuyen.length > 0) {
-            console.log(bacTaiTrucTuyen[0] + '-----------------')
-            for(var i = 0; i<bacTaiTrucTuyen.length; i++) {
-                console.log(bacTaiTrucTuyen[i] + '@@@@@@@@@@@@@@@@@@')
-                var id = bacTaiTrucTuyen[i].id
-                var username = bacTaiTrucTuyen[i].username
-                var viDo = bacTaiTrucTuyen[i].viDo
-                var kinhDo = bacTaiTrucTuyen[i].kinhDo
-                var khoangCach = ''
-                
-                axios.get(`https://router.project-osrm.org/route/v1/driving/${bacTaiTrucTuyen[i].kinhDo},${bacTaiTrucTuyen[i].viDo};${data.noiDonKhach_kinhDo},${data.noiDonKhach_viDo}`)
-                .then(res => {
-                    khoangCach = res.data.routes[0].distance
+            bacTaiTrucTuyen.forEach(function(item, index, array) {
+                let khoangCachBacTaiVaKH = geolib.getDistance (
+                    { latitude: data.viDoDi_KH, longitude: data.kinhDoDi_KH },
+                    { latitude: item.viDo_BT, longitude: item.kinhDo_BT }
+                )
+                danhSachBacTai.push({
+                    id: item.id,
+                    sdt_BT: item.sdt_BT,
+                    viDo_BT: item.viDo_BT,
+                    kinhDo_BT: item.kinhDo_BT,
+                    khoangCach: khoangCachBacTaiVaKH,
                 })
-                .catch(err => {
-                    console.log('Loi: ' + err)
-                })
-                danhSachKhoangCach.push({
-                    id: id,
-                    username: username,
-                    viDo: viDo,
-                    kinhDo: kinhDo,
-                    khoangCach: khoangCach
-                })
-                console.log(danhSachKhoangCach[0] + 'danh sach khoang cach')
-
-            }
-            var bacTaiGanNhat = TimBacTaiGanNhat(danhSachKhoangCach)
-            console.log(bacTaiGanNhat + 'bac tai gan nhat')
-            io.to(bacTaiGanNhat.id).emit('yeucaudatxe', data)
+            })
+            var bacTaiGanNhat = TimBacTaiGanNhat(danhSachBacTai)
+            console.log('Thông tin bác tài gần nhất: ' + bacTaiGanNhat.id)
+    
+            io.to(bacTaiGanNhat.id).emit('yeucauxe', {
+                id_KH: socket.id,
+                sdt_KH: data.sdt_KH,
+                viDoDi_KH: data.viDoDi_KH,
+                kinhDoDi_KH: data.kinhDoDi_KH,
+                viTriDiemDi_KH: data.viTriDiemDi_KH,
+                viDoDen_KH: data.viDoDen_KH,
+                kinhDoDen_KH: data.kinhDoDen_KH,
+                viTriDiemDen_KH: data.viTriDiemDen_KH,  
+            })
+            
         }
-        */
-        
-        
+        else {
+            socket.emit('khongcobactai')
+        }
+
     })
     
 })
 
 TimBacTaiGanNhat = (mang) => {
-    let ganNhat = mang[0].khoangCach
+    let ganNhat = mang[0]
     for(let i = 0; i<mang.length; i++) {
-        if(mang[i].khoangCach<ganNhat) {
+        if(mang[i].khoangCach<ganNhat.khoangCach) {
             ganNhat = mang[i]
         }
     }
     return ganNhat //trả về bác tài gần nhất
 }
 
+xoaKhoiDanhSach = (id) => {
+    let xoa = -1
+    bacTaiTrucTuyen.forEach( (item, index, array) => {
+        if(id === item.id) {
+            xoa = bacTaiTrucTuyen.indexOf(item)
+        }
+    })
+    
+    if(xoa !== -1) {
+        bacTaiTrucTuyen.splice(xoa, 1)
+    }
+    
+}
 
 server.listen(PORT, () => {console.log("Server started on http://localhost:"+PORT)})
 
